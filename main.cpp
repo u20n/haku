1eb75b7425869a75d74bac024fa993d32ecb39e3
apius <notyoudev@gmail.com>
2022-07-22 21:24:55 -0400

    remove old

--- main.cpp
+++ main.cpp
@@ -5,30 +5,10 @@
 #include "inc/screen.hpp"
 #include "inc/types/inputl.hpp"
 
-/**
- * push a message to nodes
- *
- *
- * */
-void push(std::string msg) {
-
-}
-
-
 int main(void) {
-  /** 
-   * content buffer 
-   * we compute the visible portion of this @ runtime
-   * 
-   * message buffer
-   * input buffer for user
-   * */
-  std::vector<std::string> cbuff;
-  
   inputl mbar;
   bool close = false; // close flag
-  unsigned int cindex = cbuff.size(); // index of the lowest "visible" message
-
+  
   char mode[2] = {'~', '>'};
   initscr();
   keypad(stdscr, TRUE); // enable function keys
