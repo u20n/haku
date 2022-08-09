61dc0c00f7b59c16858f70992182cc904fa107c2
apius <notyoudev@gmail.com>
2022-08-09 16:34:40 -0400

    refine push functionality

--- inc/push.hpp
+++ inc/push.hpp
@@ -7,56 +7,34 @@
 
 /**
  * this provides filesystem information 
- * needed to generate a COBS block
+ * needed to generate a COBS+ block
  * we leave the creation of this block to an
- * agent implementation to avoid
+ * overlay implementation to avoid
  * unneeded complexity here
+ * ~
+ * this is to reduce code repetion across
+ * seperate overlay implementation
  * */
-void push(
-    std::function<
-      std::array<std::string, 2>
-      (std::vector<std::string>)
-    > bgen, 
-    std::string bdir
-  ) {
-  /** 
-   * pull the most recently written files
-   * from the block directory 
-   * we assume 3 parents here, but this can be updated
-   * and read from a config file later
-   *
-   *
-   * we look at relative time between blocks 
-   * */
-  try {
-    std::vector<std::filesystem::directory_entry> top;
-    for (auto const& block: std::filesystem::directory_iterator{bdir}) {
-      for (unsigned int i=top.size()-1; i!=0; i--) {
-        if (std::filesystem::last_write_time(top.at(i)) > std::filesystem::last_write_time(block)) {
-          top.insert(top.begin()+i, block);
+
+namespace bfs {
+  std::vector<std::string> recent(unsigned int depth, std::string fspath) {
+    try {
+      std::vector<std::filesystem::directory_entry> top;
+      top.resize(depth);
+      for (const auto& block: std::filesystem::directory_iterator{fspath}) {
+        for (unsigned int i=top.size()-1; i!=0; i--) {
+          if (std::filesystem::last_write_time(top.at(i)) > std::filesystem::last_write_time(block)) {
+            top.at(i) = block;
+          }
         }
       }
+      std::vector<std::string> r;
+      for (const auto& entry: top) {
+        r.push_back(entry.path().stem());
+      }
+      return r;
+    } catch (std::exception& e) {
+      // log
     }
-    // prune
-    // v see above comment
-    unsigned int COUNT = 3;
-    if (top.size() > COUNT) {
-      top.erase(top.end()-COUNT, top.begin());
-    }
-
-    /** fill out parents */
-    std::vector<std::string> parents;
-    for (const auto& entry: top) {
-      parents.push_back(entry.path().stem());
-    }
-    
-    // TODO: write to new block
-    auto cont = bgen(parents);
-    std::ofstream nblock(bdir+cont[0]+".block");
-    nblock << cont[1].c_str();
-    nblock.close();
-  } catch (std::exception& e) {
-    // TODO: log
-    base::log("./logfile.log", e.what());
   }
- }
+};
